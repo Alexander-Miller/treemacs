1766528232b95b219c7f9465011c6aca8a24b62c
Alexander Miller <alexanderm@web.de>
2016-07-29 07:21:46 +0200

    Add license header.

--- treemacs.el
+++ treemacs.el
@@ -1,4 +1,25 @@
-;;; treemacs --- An emacs file viewer
+;;; treemacs.el --- A tree style file viewer package
+
+;; Copyright (C) 2016 Alexander Miller
+
+;; Author: Alexander Miller <alexanderm@web.de>
+;; Package-Requires: ((cl-lib "0.5") (dash "2.11.0") (s "1.10.0") (f "0.11.0") (ace-window "0.9.0"))
+;; Homepage: https://github.com/Alexander-Miller/treemacs
+;; Version: 1.0
+;; Keywords: tree, file
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
