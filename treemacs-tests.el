6c6b1f8f0303759ef8f1a9cb4f7bfdf7751eb975
Alexander M <alexanderm@web.de>
2017-03-30 07:51:31 +0200

    Fix compiler warnings in tests.

--- treemacs-tests.el
+++ treemacs-tests.el
@@ -1,4 +1,33 @@
+;;; treemacs.el --- A tree style file viewer package
+
+;; Copyright (C) 2017 Alexander Miller
+
+;; Author: Alexander Miller <alexanderm@web.de>
+;; Package-Requires: ((cl-lib "0.5") (dash "2.11.0") (s "1.10.0") (f "0.11.0") (ace-window "0.9.0"))
+;; Homepage: https://github.com/Alexander-Miller/treemacs
+;; Version: 1.0
+;; Keywords: tree, file, explorer
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; Tests extracted into their own file to reduce clutter.
+
+;;; Code:
+
 (require 'treemacs-impl)
+(require 'treemacs-customization)
 (require 'ert)
 (require 'el-mock)
 
@@ -144,6 +173,8 @@
               ("/home/A/B/C1/D3" "/home/A/B/C1/D3/E1")
               ("/home/A2" "/home/A2/B1" "/home/A2/B2"))))
       (treemacs--clear-from-cache "/home/A/B" t)
-      (should (equal treemacs--open-dirs-cache '(("/home/A2" "/home/A2/B1" "/home/A2/B2"))))))
+      (should (equal treemacs--open-dirs-cache '(("/home/A2" "/home/A2/B1" "/home/A2/B2")))))))
 
-)
+(provide 'treemacs-tests)
+
+;;; treemacs-tests.el ends here
