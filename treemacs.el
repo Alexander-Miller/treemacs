a0a1fcf45e78e995078d9de6a5006b41fd597168
Alexander M <alexanderm@web.de>
2017-05-06 13:49:52 +0200

    Add typescript icon.

--- treemacs.el
+++ treemacs.el
@@ -5,7 +5,7 @@
 ;; Author: Alexander Miller <alexanderm@web.de>
 ;; Package-Requires: ((cl-lib "0.5") (dash "2.11.0") (s "1.10.0") (f "0.11.0") (ace-window "0.9.0"))
 ;; Homepage: https://github.com/Alexander-Miller/treemacs
-;; Version: 1.0
+;; Version: 1.2.4
 ;; Keywords: tree, file, explorer
 
 ;; This program is free software; you can redistribute it and/or modify
