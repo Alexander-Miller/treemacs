ad3948708208c69a5c28ebe8297d7fa2ad501523
Alexander Miller <alexanderm@web.de>
2018-04-17 07:29:05 +0200

    Put python scripts into methods.
    Supposedly this is better for performance.

--- src/scripts/treemacs-dirs-to-collapse.py
+++ src/scripts/treemacs-dirs-to-collapse.py
@@ -4,6 +4,13 @@ from posixpath import join
 import sys
 import os
 
+ROOT     = sys.argv[1]
+LIMIT    = int(sys.argv[2])
+SHOW_ALL = sys.argv[3] == 't'
+
+if LIMIT <= 0:
+    exit(0)
+
 def dir_content(path):
     ret = []
     for item in listdir(path):
@@ -12,34 +19,30 @@ def dir_content(path):
             ret.append(full_path)
     return ret
 
-ROOT     = sys.argv[1]
-LIMIT    = int(sys.argv[2])
-SHOW_ALL = sys.argv[3] == 't'
-dirs     = [d for d in dir_content(ROOT) if isdir(d)]
-out      = sys.stdout
-
-if LIMIT <= 0:
-    exit(0)
-
-out.write("(")
-for current_dir in dirs:
-    content   = dir_content(current_dir)
-    collapsed = current_dir
-    steps     = []
-    depth     = 0
-    while True:
-        if len(content) == 1 and isdir(content[0]):
-            single_path = content[0]
-            collapsed   = join(collapsed, single_path)
-            content     = dir_content(collapsed)
-            depth      += 1
-            steps.append(single_path)
-            if depth >= LIMIT:
+def main():
+    out  = sys.stdout
+    dirs = [d for d in dir_content(ROOT) if isdir(d)]
+    out.write("(")
+    for current_dir in dirs:
+        content   = dir_content(current_dir)
+        collapsed = current_dir
+        steps     = []
+        depth     = 0
+        while True:
+            if len(content) == 1 and isdir(content[0]):
+                single_path = content[0]
+                collapsed   = join(collapsed, single_path)
+                content     = dir_content(collapsed)
+                depth      += 1
+                steps.append(single_path)
+                if depth >= LIMIT:
+                    break
+            else:
                 break
-        else:
-            break
-    if depth > 0:
-        final_dir      = steps[-1]
-        display_suffix = final_dir[len(current_dir):]
-        out.write("(" + '"' + current_dir + '" ' + '"' + display_suffix + '" ' + '"' + '" "'.join(steps) + '")')
-out.write(")")
+        if depth > 0:
+            final_dir      = steps[-1]
+            display_suffix = final_dir[len(current_dir):]
+            out.write("(" + '"' + current_dir + '" ' + '"' + display_suffix + '" ' + '"' + '" "'.join(steps) + '")')
+    out.write(")")
+
+main()
