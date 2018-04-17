43a86265b380ea1056b0356cfa2a33665bd5be34
Alexander Miller <alexanderm@web.de>
2018-04-17 07:29:05 +0200

    Put python scripts into methods.
    Supposedly this is better for performance.

--- src/scripts/treemacs-git-status.py
+++ src/scripts/treemacs-git-status.py
@@ -11,9 +11,6 @@ OPEN      = b'("'
 CLOSE     = b'")'
 CONS      = b'" . "'
 
-proc = Popen(GIT_CMD, shell=True, stdout=PIPE, bufsize=100)
-dirs = {}
-
 def print_all_untracked_files(path):
     for item in listdir(path):
         full_path = join(path, item)
@@ -22,42 +19,48 @@ def print_all_untracked_files(path):
         if isdir(full_path):
             print_all_untracked_files(full_path)
 
-STDOUT.write(b'(')
-for item in proc.stdout:
-    if item.startswith(b' '):
-        item = item[1:]
-    state, filename = item.split(b' ', 1)
+def main():
+    proc = Popen(GIT_CMD, shell=True, stdout=PIPE, bufsize=100)
+    dirs = {}
 
-    # renames have the form STATE OLDNAME -> NEWNAME
-    # final newline must be trimmed as well
-    if state == b"R":
-        full_root = join(GIT_ROOT, filename.split(b' -> ')[1][:-1])
-    else:
-        full_root = join(GIT_ROOT, filename.lstrip()[:-1])
+    STDOUT.write(b'(')
+    for item in proc.stdout:
+        if item.startswith(b' '):
+            item = item[1:]
+        state, filename = item.split(b' ', 1)
 
-    # filename is a directory, final slash must be removed
-    if full_root.endswith(b'/'):
-        full_root = full_root[:-1]
-        STDOUT.write(OPEN + state + CONS + full_root + CLOSE)
-        # STDOUT.write(b'\n')
-        dirs[full_root] = True
-    else:
-        STDOUT.write(OPEN + state + CONS + full_root + CLOSE)
-        # STDOUT.write(b'\n')
-    # for files deeper down in the file hierarchy also print all their directories
-    # if ./foo/bar/baz.el is changed then ./foo and ./foo/bar must be shown as changed as well
-    if b'/' in filename and state != b"!!":
-        name_parts = filename.split(b'/')[:-1]
-        dirname = b''
-        for name_part in name_parts:
-            dirname = join(dirname, name_part)
-            full_dirname = join(GIT_ROOT, dirname.lstrip())
-            # directories should not be printed more than once, which would happen if
-            # e.g. both ./foo/x and ./foo/y have changes
-            if full_dirname not in dirs:
-                STDOUT.write(OPEN + b'M' + CONS + full_dirname + CLOSE)
-                # STDOUT.write(b'\n')
-                dirs[full_dirname] = True
-    if state.startswith(b'?') and isdir(full_root):
-        print_all_untracked_files(full_root)
-STDOUT.write(b')')
+        # renames have the form STATE OLDNAME -> NEWNAME
+        # final newline must be trimmed as well
+        if state == b"R":
+            full_root = join(GIT_ROOT, filename.split(b' -> ')[1][:-1])
+        else:
+            full_root = join(GIT_ROOT, filename.lstrip()[:-1])
+
+        # filename is a directory, final slash must be removed
+        if full_root.endswith(b'/'):
+            full_root = full_root[:-1]
+            STDOUT.write(OPEN + state + CONS + full_root + CLOSE)
+            # STDOUT.write(b'\n')
+            dirs[full_root] = True
+        else:
+            STDOUT.write(OPEN + state + CONS + full_root + CLOSE)
+            # STDOUT.write(b'\n')
+        # for files deeper down in the file hierarchy also print all their directories
+        # if ./foo/bar/baz.el is changed then ./foo and ./foo/bar must be shown as changed as well
+        if b'/' in filename and state != b"!!":
+            name_parts = filename.split(b'/')[:-1]
+            dirname = b''
+            for name_part in name_parts:
+                dirname = join(dirname, name_part)
+                full_dirname = join(GIT_ROOT, dirname.lstrip())
+                # directories should not be printed more than once, which would happen if
+                # e.g. both ./foo/x and ./foo/y have changes
+                if full_dirname not in dirs:
+                    STDOUT.write(OPEN + b'M' + CONS + full_dirname + CLOSE)
+                    # STDOUT.write(b'\n')
+                    dirs[full_dirname] = True
+        if state.startswith(b'?') and isdir(full_root):
+            print_all_untracked_files(full_root)
+    STDOUT.write(b')')
+
+main()
