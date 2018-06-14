fc4309bfe3c835700204ebcfeba491ca86046866
Alexander Miller <alexanderm@web.de>
2018-06-14 19:17:49 +0200

    Buffer git status process output.

--- src/scripts/treemacs-git-status.py
+++ src/scripts/treemacs-git-status.py
@@ -10,20 +10,22 @@ STDOUT    = sys.stdout.buffer
 OPEN      = b'("'
 CLOSE     = b'")'
 CONS      = b'" . "'
+output    = b""
 
 def print_all_untracked_files(path):
+    global output
     for item in listdir(path):
         full_path = join(path, item)
-        STDOUT.write(OPEN + b'?' + CONS + full_path + CLOSE)
-        # STDOUT.write(b'\n')
+        output += OPEN + b'?' + CONS + full_path + CLOSE
         if isdir(full_path):
             print_all_untracked_files(full_path)
 
 def main():
+    global output
     proc = Popen(GIT_CMD, shell=True, stdout=PIPE, bufsize=100)
     dirs = {}
 
-    STDOUT.write(b'(')
+    output += b'('
     for item in proc.stdout:
         if item.startswith(b' '):
             item = item[1:]
@@ -39,12 +41,10 @@ def main():
         # filename is a directory, final slash must be removed
         if full_root.endswith(b'/'):
             full_root = full_root[:-1]
-            STDOUT.write(OPEN + state + CONS + full_root + CLOSE)
-            # STDOUT.write(b'\n')
+            output += OPEN + state + CONS + full_root + CLOSE
             dirs[full_root] = True
         else:
-            STDOUT.write(OPEN + state + CONS + full_root + CLOSE)
-            # STDOUT.write(b'\n')
+            output += OPEN + state + CONS + full_root + CLOSE
         # for files deeper down in the file hierarchy also print all their directories
         # if ./foo/bar/baz.el is changed then ./foo and ./foo/bar must be shown as changed as well
         if b'/' in filename and state != b"!!":
@@ -56,11 +56,11 @@ def main():
                 # directories should not be printed more than once, which would happen if
                 # e.g. both ./foo/x and ./foo/y have changes
                 if full_dirname not in dirs:
-                    STDOUT.write(OPEN + b'M' + CONS + full_dirname + CLOSE)
-                    # STDOUT.write(b'\n')
+                    output += OPEN + b'M' + CONS + full_dirname + CLOSE
                     dirs[full_dirname] = True
         if state.startswith(b'?') and isdir(full_root):
             print_all_untracked_files(full_root)
-    STDOUT.write(b')')
+    output += b')'
+    STDOUT.write(output)
 
 main()
