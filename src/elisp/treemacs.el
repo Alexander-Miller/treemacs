c3b52086068f7ca838f31e51724498eada26d354
Alexander Miller <alexanderm@web.de>
2019-01-27 15:20:39 +0100

    [Misc] Bump version.

--- src/elisp/treemacs.el
+++ src/elisp/treemacs.el
@@ -5,7 +5,7 @@
 ;; Author: Alexander Miller <alexanderm@web.de>
 ;; Package-Requires: ((emacs "25.2") (cl-lib "0.5") (dash "2.11.0") (s "1.10.0") (f "0.11.0") (ace-window "0.9.0") (pfuture "1.2") (hydra "0.13.2") (ht "2.2"))
 ;; Homepage: https://github.com/Alexander-Miller/treemacs
-;; Version: 2.3
+;; Version: 2.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -58,7 +58,7 @@
 
 (defconst treemacs-version
   (eval-when-compile
-    (format "v2.3-%s @ %s"
+    (format "v2.4-%s @ %s"
             (format-time-string "%Y.%m.%d" (current-time))
             emacs-version)))
 
