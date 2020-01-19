fddb401c1a5fce6e6fd320b6a2948f5f387d3c08
Alexander Miller <alexanderm@web.de>
2020-01-19 12:39:00 +0100

    [Diagnostics] Preview implementation.

---
+++ src/elisp/treemacs-diagnostics.el
@@ -0,0 +1,63 @@
+;;; treemacs-diagnostics.el --- A tree style file viewer package -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Alexander Miller
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; WIP implementation of diagnostics display.
+
+;;; Code:
+
+(require 'dash)
+(require 'overlay)
+(require 'treemacs-dom)
+(require 'treemacs-macros)
+(require 'treemacs-scope)
+(require 'treemacs-workspaces)
+(require 'treemacs-core-utils)
+
+(defconst treemacs--diag-store (make-hash-table :size 300 :test 'equal))
+
+(defface treemacs-diagnostic-error
+  '((t :underline "red"))
+  "TODO"
+  :group 'treemacs-faces)
+
+(defface treemacs-diagnostic-warning
+  '((t :underline "yellow"))
+  "TODO"
+  :group 'treemacs-faces)
+
+(defun treemacs-apply-diagnostics (path &rest diagnostics)
+  "Display DIAGNOSTICS for given PATH."
+  (declare (indent 1))
+  (treemacs-run-in-every-buffer
+   (save-excursion
+     (-when-let (dom-node (treemacs-find-in-dom path))
+       (let* ((start (treemacs-dom-node->position dom-node))
+              (end (treemacs--next-neighbour-of start)))
+         (-each (overlays-in start end) #'delete-overlay))
+       (while diagnostics
+         (let ((path  (pop diagnostics))
+               (state (pop diagnostics)))
+           (when  (treemacs-is-path-visible? path)
+             (-let [btn (treemacs-find-file-node path)]
+               (-doto (make-overlay (treemacs-button-start btn) (treemacs-button-end btn))
+                 (overlay-put 'face state))))))))
+   (hl-line-highlight)))
+
+(provide 'treemacs-diagnostics)
+
+;;; treemacs-diagnostics.el ends here
