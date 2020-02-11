887a4d64928c1028d564f220eab776692f75e93a
Alexander Miller <alexanderm@web.de>
2020-02-11 17:05:52 +0100

    [Diagnostics] Use a thunk.el based argument provider.

--- src/elisp/treemacs-diagnostics.el
+++ src/elisp/treemacs-diagnostics.el
@@ -21,6 +21,7 @@
 ;;; Code:
 
 (require 'ht)
+(require 'thunk)
 (require 'dash)
 (require 'overlay)
 (require 'treemacs-dom)
@@ -37,12 +38,12 @@
 (defconst treemacs--apply-diagnostics-delay 3
   "Debounce delay for the application of diagnostics.")
 
-(defface treemacs-diagnostic-error
+(defface treemacs-diagnostic-error-face
   '((t :underline "red"))
   "TODO"
   :group 'treemacs-faces)
 
-(defface treemacs-diagnostic-warning
+(defface treemacs-diagnostic-warning-face
   '((t :underline "yellow"))
   "TODO"
   :group 'treemacs-faces)
@@ -57,17 +58,33 @@
     (while diagnostics
       (ht-set! ht (pop diagnostics) (pop diagnostics)))))
 
-(defun treemacs-apply-diagnostics (path &rest diagnostics)
-  "Display DIAGNOSTICS for given PATH."
-  (declare (indent 1))
+(defun treemacs-apply-diagnostics (provider)
+  "Display diagnostics based on the given arguments PROVIDER.
+PROVIDER should be a `thunk' (see thunk.el) that, when evaluated, returns a flat
+list of consecutive path and face items.
+
+File paths should use treemacs' canonical format - they should be absolute,
+expanded and *not* have a trailing slash.
+
+The diagnostic faces could be any face symbols or raw face literals. Treemacs
+features several pre-made faces named `treemacs-diagnostic-*-face'.
+
+This method is debounced, it will never run more often than once every 3
+seconds. In addition the use of a lazy thunk ensures that potentially expensive
+transformations happen only once and only when required. Performance is thus not
+expected to be a major issue.
+
+A basic example use would look like this:
+
+\(treemacs-apply-diagnostics
+    (thunk-delay '(\"/path/to/file/x\" 'treemacs-diagnostic-warning-face
+                   \"/path/to/file/y\" 'treemacs-diagnostic-error-face
+                   \"/path/to/file/z\" '((:underline \"green\")))))"
   (treemacs-debounce treemacs--diagnostic-timer treemacs--apply-diagnostics-delay
-    (treemacs--reset-and-save-diagnosics path diagnostics)
     (treemacs-run-in-every-buffer
      (save-excursion
-       (-when-let (dom-node (treemacs-find-in-dom path))
-         (let* ((start (treemacs-dom-node->position dom-node))
-                (end (treemacs--next-neighbour-of start)))
-           (-each (overlays-in start end) #'delete-overlay))
+       (-each (overlays-in (point-min) (point-max)) #'delete-overlay)
+       (-let [diagnostics (thunk-force provider)]
          (while diagnostics
            (let ((path  (pop diagnostics))
                  (state (pop diagnostics)))
