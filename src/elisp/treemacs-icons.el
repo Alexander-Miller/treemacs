ed7de5d0a25b19d9ad5f376b5e447123b418964b
Alexander Miller <alexanderm@web.de>
2020-10-26 21:06:59 +0100

    [Icons] Use the hl-line-face custom variable to grab hl-line's background.

--- src/elisp/treemacs-icons.el
+++ src/elisp/treemacs-icons.el
@@ -112,7 +112,7 @@ Fetch the current Emacs theme's background & hl-line colours and inject them
 into the gui icons of every theme in `treemacs--themes'.
 Also called as advice after `load-theme', hence the ignored argument."
   (let* ((default-background (face-attribute 'default :background nil t))
-         (hl-line-background (face-attribute 'hl-line :background nil t))
+         (hl-line-background (face-attribute hl-line-face :background nil t))
          (test-icon          (treemacs-get-icon-value 'dir-open))
          (icon-background    (treemacs--get-img-property (get-text-property 0 'img-unselected test-icon) :background))
          (icon-hl-background (treemacs--get-img-property (get-text-property 0 'img-selected test-icon) :background)))
