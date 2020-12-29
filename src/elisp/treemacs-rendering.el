d1f46c41df7b0786d603ac479a9d13d22f06ed0b
Alexander Miller <alexanderm@web.de>
2020-12-29 14:57:22 +0100

    [Icons] Add distinct icons for closed and open project root nodes.
    Fixes #452

--- src/elisp/treemacs-rendering.el
+++ src/elisp/treemacs-rendering.el
@@ -497,6 +497,7 @@ Run POST-CLOSE-ACTION after everything else is done."
            :immediate-insert nil
            :button btn
            :new-state 'root-node-open
+           :new-icon treemacs-icon-root-open
            :open-action
            (progn
              ;; TODO(2019/10/14): go back to post open
@@ -517,6 +518,7 @@ Remove all open entries below BTN when RECURSIVE is non-nil."
   (treemacs--button-close
    :button btn
    :new-state 'root-node-closed
+   :new-icon treemacs-icon-root-closed
    :post-close-action
    (-let [path (treemacs-button-get btn :path)]
      (treemacs--stop-watching path)
@@ -581,7 +583,7 @@ Remove all open dir and tag entries under BTN when RECURSIVE."
   "Insert a new root node for the given PROJECT node.
 
 PROJECT: Project Struct"
-  (insert treemacs-icon-root)
+  (insert treemacs-icon-root-closed)
   (let* ((pos (point-marker))
          (path (treemacs-project->path project))
          (dom-node (treemacs-dom-node->create! :key path :position pos)))
