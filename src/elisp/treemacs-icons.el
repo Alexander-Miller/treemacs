967916c3fcab21750437724060565cc3382b8c78
Alexander Miller <alexanderm@web.de>
2023-09-25 15:27:22 +0200

    [Icons] Correct the determination of icons' types.
    Fixes #1061

--- src/elisp/treemacs-icons.el
+++ src/elisp/treemacs-icons.el
@@ -197,8 +197,11 @@ Necessary since root icons are not rectangular."
          (treemacs--root-icon-size-adjust width height))
        (if (and (integerp treemacs--icon-size) (image-type-available-p 'imagemagick))
            (create-image ,file-path 'imagemagick nil :ascent 'center :width width :height height)
-         (create-image ,file-path (intern (treemacs--file-extension ,file-path)) nil
-                       :ascent 'center :width width :height height))))))
+         (create-image
+          ,file-path
+          (intern (treemacs--file-extension (treemacs--filename ,file-path)))
+          nil
+          :ascent 'center :width width :height height))))))
 
 (define-inline treemacs--create-icon-strings (file fallback)
   "Create propertized icon strings for a given FILE image and TUI FALLBACK."
